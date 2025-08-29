05edc291d3be317a64c3cdca61d71f7edaed711e
Poorab <poorab98@gmail.com>
2025-08-29 12:53:30 +0530

    foler structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#creating dir
+mkdir -p src
+mkdir -p research
+
+#creating file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch reqirements.txt
+
+
+echo "Dir and Files created successfully"
